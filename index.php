264060cb09f90cc2faec6eced770e628a892dd7d
PietroDub <pietro.henriquevito@gmail.com>
2025-08-16 08:47:44 -0300

    Criando repositório e arquivos

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+ //verificação com if
+     echo("Você digitou essas informações!");
+     echo $_GET ["nome"];
+     echo("<br>");
+     echo $_GET ["email"];
+     echo("<br>");
+     echo $_GET ["telefone"];
+?>
